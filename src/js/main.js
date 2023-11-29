f84ccb3a38391d0d6b98f50f2edf25d62856fd30
benjamin <benjamin.marissiaux@student.hepl.be>
2023-11-29 10:04:53 +0100

    Day and Night

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+/*Au clic de l’élément .tumbler__wrapper vous devez ajouter les classes
+body--night-mode à l'élément body
+tumbler--night-mode à l’élément .tumbler
+À tous les éléments .post la classe post--night-mode
+Quand on clique une seconde fois il faut retirer les classes que vous venez d'ajouter*/
+
+const  settings = {
+    jsEnabledClass : 'js-enabled',
+    bodyNightModeClass : 'body--night-mode',
+    tumblerSelector : '.tumbler',
+    tumblerNightClass : 'tumbler--night-mode',
+    postNightClass : 'post--night-mode',
+    postSelector : '.post'
+};
+
+document.documentElement.classList.add(settings.jsEnabledClass);
+const buttonSelect = document.querySelector('.tumbler__wrapper');
+buttonSelect.addEventListener('click', ()=>{
+    document.body.classList.toggle(settings.bodyNightModeClass);
+    buttonSelect.querySelector(settings.tumblerSelector).classList.toggle(settings.tumblerNightClass);
+    for(const post of document.querySelectorAll(settings.postSelector)){
+        post.classList.toggle(settings.postNightClass);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
