f4b223e971c9b8cb5ae8e9464ae0836c2e62843a
benjamin <benjamin.marissiaux@student.hepl.be>
2024-01-14 14:19:55 +0100

    Day and Night

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ tumbler--night-mode à l’élément .tumbler
 À tous les éléments .post la classe post--night-mode
 Quand on clique une seconde fois il faut retirer les classes que vous venez d'ajouter*/
 
-const  settings = {
+/*const  settings = {
     jsEnabledClass : 'js-enabled',
     bodyNightModeClass : 'body--night-mode',
     tumblerSelector : '.tumbler',
@@ -21,7 +21,54 @@ buttonSelect.addEventListener('click', ()=>{
     for(const post of document.querySelectorAll(settings.postSelector)){
         post.classList.toggle(settings.postNightClass);
     }
-});
+});*/
+
+
+const settings = {
+    jsEnabledClass: 'js-enabled',
+    tumblerSelector: '.tumbler',
+    tumblerNightMode: 'tumbler--night-mode',
+    btn: '.tumbler__wrapper',
+    bodyNightMode: 'body--night-mode',
+    articlePostNightMode: 'post--night-mode',
+    postSelector : '.post'
+}
+
+const nightMode = {
+    init(){
+        document.querySelector('html').classList.add(settings.jsEnabledClass);
+        this.addEventListeners();
+    },
+    addEventListeners(){
+        document.querySelector(settings.btn).addEventListener('click', () => {
+            document.body.classList.toggle(settings.bodyNightMode);
+            document.querySelector(settings.tumblerSelector).classList.toggle(settings.tumblerNightMode);
+            document.querySelectorAll(settings.postSelector).forEach(postElement => {
+                postElement.classList.add(settings.articlePostNightMode);
+            });
+        });
+    }
+}
+
+nightMode.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
